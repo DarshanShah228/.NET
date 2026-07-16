b40000d4820ea766358e601cdc4d7c1aa7ee9db3
DarshanShah228 <darshanpshah6966@gmail.com>
2026-07-16 18:17:49 +0530

    Add simple calculator practical

---
+++ simple calculator/simple calculator/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="simple_calculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Simple Calculator</h1>
+            <asp:Label ID="Lno1" runat="server" Text="Enter No1:"></asp:Label>
+            <asp:TextBox ID="Tno1" runat="server"></asp:TextBox><br /><br />
+
+            <asp:Label ID="Lno2" runat="server" Text="Enter No2:"></asp:Label>
+            <asp:TextBox ID="Tno2" runat="server"></asp:TextBox><br /><br />
+
+            <asp:Button ID="BtnAdd" runat="server" Text="Add" OnClick="BtnAdd_Click" />
+            <asp:Button ID="BtnSub" runat="server" Text="Sub" OnClick="BtnSub_Click" />
+            <asp:Button ID="BtnMulti" runat="server" Text="Multi" OnClick="BtnMulti_Click" />
+            <asp:Button ID="BtnDiv" runat="server" Text="Div" OnClick="BtnDiv_Click" />
+            <br /><br />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ simple calculator/simple calculator/WebForm1.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace simple_calculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if (Tno1.Text == "" || Tno2.Text == "")
+            {
+                Label1.Text = "Enter Number Plz";
+            }
+            else
+            {
+                int a=int.Parse(Tno1.Text);
+                int b = int.Parse(Tno2.Text);
+                int c = a + b;
+                Label1.Text = "Answer" + "<br/>" + c.ToString();
+            }
+        }
+
+        protected void BtnSub_Click(object sender, EventArgs e)
+        {
+            if (Tno1.Text == "" || Tno2.Text == "")
+            {
+                Label1.Text = "Enter Number Plz";
+            }
+            else
+            {
+                int a = int.Parse(Tno1.Text);
+                int b = int.Parse(Tno2.Text);
+                int c = a - b;
+                Label1.Text = "Answer" + "<br/>" + c.ToString();
+            }
+        }
+
+        protected void BtnMulti_Click(object sender, EventArgs e)
+        {
+            if (Tno1.Text == "" || Tno2.Text == "")
+            {
+                Label1.Text = "Enter Number Plz";
+            }
+            else
+            {
+                int a = int.Parse(Tno1.Text);
+                int b = int.Parse(Tno2.Text);
+                int c = a * b;
+                Label1.Text = "Answer" + "<br/>" + c.ToString();
+            }
+        }
+
+        protected void BtnDiv_Click(object sender, EventArgs e)
+        {
+            if (Tno1.Text == "" || Tno2.Text == "")
+            {
+                Label1.Text = "Enter Number Plz";
+            }
+            else
+            {
+                int a = int.Parse(Tno1.Text);
+                int b = int.Parse(Tno2.Text);
+                int c = a / b;
+                Label1.Text = "Answer" + "<br/>" + c.ToString();
+            }
+        }
+    }
+}
