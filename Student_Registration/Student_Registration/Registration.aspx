0222fe22fd1e275c4f0ef3fb20e3c15558273311
DarshanShah228 <darshanpshah6966@gmail.com>
2026-08-03 22:59:17 +0530

    Implementing Student Registration Pratical

---
+++ Student_Registration/Student_Registration/Registration.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="Student_Registration.Registration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Roll No : <asp:TextBox ID="rnm" runat="server" TextMode="Number"></asp:TextBox><br /><br />
+            Name : <asp:TextBox ID="snm" runat="server"></asp:TextBox><br /><br />
+            Department : <asp:DropDownList ID="dept" runat="server">
+                <asp:ListItem>BCA</asp:ListItem>
+                <asp:ListItem>MCA</asp:ListItem>
+                <asp:ListItem>BBA</asp:ListItem>
+                <asp:ListItem>MBA</asp:ListItem>
+            </asp:DropDownList><br /><br />
+            Subject : <asp:DropDownList ID="subject" runat="server">
+                <asp:ListItem>No-Code</asp:ListItem>
+                <asp:ListItem>Laravel</asp:ListItem>
+                <asp:ListItem>IOT</asp:ListItem>
+            </asp:DropDownList><br /><br />
+            Lab Batch : <asp:RadioButton ID="b1" runat="server" GroupName="batch" Checked="True" />B1
+                        <asp:RadioButton ID="b2" runat="server" GroupName="batch" />B2
+                        <asp:RadioButton ID="b3" runat="server" GroupName="batch" />B3
+            <br /><br />
+
+            <asp:Button ID="ClickSubmit" runat="server" Text="Submit" OnClick="ClickSubmit_Click" />
+
+            <br /><br />
+            <asp:Button ID="ClickShow" runat="server" Text="Show Student Data" OnClick="ClickShow_Click" />
+            <br /><br />
+            <asp:Label ID="result" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Student_Registration/Student_Registration/Registration.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Student_Registration
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        static String[] name = new string[5];
+        static int[] rollno = new int[5];
+        static String[] department = new string[5];
+        static String[] sub = new string[5];
+        static String[] batch = new string[5];
+
+        static int cnt = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ClickSubmit_Click(object sender, EventArgs e)
+        {
+            string nm = snm.Text;
+            int ro = int.Parse(rnm.Text);
+            string dp = dept.SelectedValue;
+            string sub1 = subject.SelectedValue;
+            string bt = "";
+
+            if (b1.Checked)
+                bt = "B1";
+            if (b2.Checked)
+                bt = "B2";
+            if (b3.Checked)
+                bt = "B3";
+
+            name[cnt] = nm;
+            rollno[cnt]= ro;
+            department[cnt] = dp;
+            sub[cnt] = sub1;
+            batch[cnt] = bt;
+            cnt++;
+            //for disable the button after 5 data inserted
+            if(cnt==5)
+                ClickSubmit.Enabled = false;
+        }
+
+        protected void ClickShow_Click(object sender, EventArgs e)
+        {
+            result.Text = "";
+            for (int i = 0; i < cnt; i++)
+            {
+                result.Text += "<br>Roll No : " + rollno[i] + "<br>Name : " + name[i] +
+                    "<br>Department : " + department[i] + "<br>Subject : " + sub[i] +
+                    "<br>Lab Batch : " + batch[i] + "<br><hr>";
+            }
+        }
+    }
+}
